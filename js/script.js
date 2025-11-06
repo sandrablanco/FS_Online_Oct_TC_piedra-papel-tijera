e2734bb4b9be3d05667a44508468672132c1d753
Monica Serrano <monicasesal@gmail.com>
2025-11-06 13:46:52 +0100

    Ejercicio a medias

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const opciones = ["piedra","papel","tijera"];
+let puntosUsuario = 0;
+let puntosOrdenador = 0;
+
+//cogemos elementos del DOM
+const botones = document.querySelectorAll(".boton-jugada");
+const resultados = document.getElementById("resultados");
+const marcadorUsuario = document.getElementById("contador-usuario");
+const marcadorOrdenador = document.getElementById ("contador-ordenador");
+
+//asignamos el evento de clic a cada boton
+//hasta aqui el usuario sabemos si ha cogido piedra, papel o tijera
+// hasta aqui el ordenador ha elegido piedra, papel o tijera
+// comparamos los resultados
+for (let i = 0; i < botones.length; i++) {
+    botones[i].addEventListener("click", function() {
+        let jugadaUsuario = botones[i].dataset.jugada;
+        console.log("has hecho click en", jugadaUsuario);        
+        let jugadaOrdenador = opciones[Math.floor(Math.random() * opciones.length)];
+        console.log("el ordenador ha elegido", jugadaOrdenador);
+
+        if (jugadaUsuario === jugadaOrdenador) {
+            console.log("empate");
+        } 
+        else if (
+            (jugadaUsuario === "piedra" && jugadaOrdenador === "tijera") ||
+            (jugadaUsuario === "papel" && jugadaOrdenador === "piedra") ||
+            (jugadaUsuario === "tijera" && jugadaOrdenador === "papel")
+        ) {
+            console.log("gana usuario");
+            puntosUsuario ++;
+        } 
+        else {
+            console.log("gana ordenador");
+            puntosOrdenador ++;
+        }
+        console.log("Usuario tiene " + puntosUsuario + " ordenador tiene " + puntosOrdenador)
+    }); 
+}
+//contador de puntos de usuario y ordenador
+//ordenador gana usuario +1punto 
+//usuario gana al ordenador +1punto
+//empate no se suma 0 puntos
+
+
+
